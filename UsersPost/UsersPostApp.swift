3e9114f389c493f622f30c55dd21afbbc2e22e61
Levi Lunique <levi.lunique@gmail.com>
2021-09-16 18:37:57 -0400

    Initial Commit

---
+++ UsersPost/UsersPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersPostApp.swift
+//  UsersPost
+//
+//  Created by user204006 on 9/16/21.
+//
+
+import SwiftUI
+
+@main
+struct UsersPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
